ef48f0043e3552836051423173837bf947590648
voncardoso <vonharrison26@gmail.com>
2023-01-15 23:13:01 -0300

    finished theme settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/theme/default";
+
 export function App() {
-  return <h1>Hello Word</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>Hello word</h1>
+    </ThemeProvider>
+  );
 }
---
+++ src/styles/theme/default.ts
@@ -0,0 +1,13 @@
+export const defaultTheme = {
+  white: "#fff",
+  backgorund: "#F7F7F7",
+
+  "gray-500": "#7A7D84",
+  "gray-900": "#1D1D1F",
+
+  "green-500": "#1BAC4B",
+
+  "red-300": "#F75A68",
+  "red-500": "#AB222E",
+  "red-700": "#7A1921",
+} as const;
