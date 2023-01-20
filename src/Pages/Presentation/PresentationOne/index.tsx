de6ed3afdbbed2e69b4c681b6ac206c910b7a5ac
Adriel Escorcio <adrielescorcio@gmail.com>
2023-01-20 00:08:46 -0300

    feat: Created sreen presentation one

--- src/Pages/Presentation/PresentationOne/index.tsx
+++ src/Pages/Presentation/PresentationOne/index.tsx
@@ -1,3 +1,16 @@
+import { ButtonApresentaion } from "../../../components/ButtonApresentaion";
+import { Main, Title, Text, Paragraph } from "./style";
+
 export function PresentationOne() {
-  return <h1>PresentationOne</h1>;
+  return <Main>
+            <img src="src\assets\Price-cuate 1.svg" alt="">
+
+            </img>
+            <Text>
+              <Title>Crie Suas Rifas</Title>
+              <Paragraph>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever </Paragraph>
+              < ButtonApresentaion path="/apresentacao2"/>
+            </Text>
+            
+          </Main>;
 }
--- src/Pages/Presentation/PresentationOne/style.ts
+++ src/Pages/Presentation/PresentationOne/style.ts
@@ -1,1 +1,34 @@
 import styled from "styled-components";
+
+
+export const Main = styled.div`
+
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100vh;
+flex-direction: column;
+padding: 1.25rem;
+
+`
+export const Text = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.25rem;
+  align-items: center;
+  padding: 0 0.3125rem;
+
+`
+export const Title = styled.h1`
+
+color: ${(props) => props.theme["green-500"]};
+
+
+`
+export const Paragraph = styled.p`
+
+text-align: center;
+margin-bottom: 3.925rem;
+font-size: 0.875em;
+
+`
