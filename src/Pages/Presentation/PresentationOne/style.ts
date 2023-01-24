6adb6475e74872a3fec561efbec6b6eb953c7e8f
Adriel Escorcio <adrielescorcio@gmail.com>
2023-01-23 21:13:10 -0300

    refactor: page styling

--- src/Pages/Presentation/PresentationOne/style.ts
+++ src/Pages/Presentation/PresentationOne/style.ts
@@ -2,33 +2,39 @@ import styled from "styled-components";
 
 
 export const Main = styled.div`
-
+max-width: 800px;
+width: 100%;
+margin: 0 auto;
 display: flex;
 justify-content: center;
 align-items: center;
 height: 100vh;
 flex-direction: column;
-padding: 1.25rem;
+padding: 0 18px;
+`
+
+export const Img = styled.img`
+
+max-width: 365px;
+width: 100%;
 
 `
 export const Text = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1.25rem;
-  align-items: center;
-  padding: 0 0.3125rem;
+  margin-bottom: 3.75rem;
+  padding: 0px;
 
 `
 export const Title = styled.h1`
 
+font-size: 2rem;
+text-align: center;
+margin-top: 1.875rem;
 color: ${(props) => props.theme["green-500"]};
 
 
 `
 export const Paragraph = styled.p`
-
 text-align: center;
-margin-bottom: 3.925rem;
+margin-bottom: 5.25rem;
 font-size: 0.875em;
-
 `
