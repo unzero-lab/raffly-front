db62f73c23e5f87e9ab437c965a6e40f7674631a
Joao Marcos <joaomarcosgnr@hotmail.com>
2023-01-26 00:02:41 -0300

    feat-new-screen-presentation-three

--- src/Pages/Presentation/PresentationThree/index.tsx
+++ src/Pages/Presentation/PresentationThree/index.tsx
@@ -1,3 +1,17 @@
-export function PresentationThree() {
-  return <h1>PresentationThree</h1>;
+import ImgApresetationThree from "../../../assets/Winners-cuate 1.svg";
+import { ButtonApresentaion } from "../../../components/ButtonApresentaion";
+import { Img, Main, Paragraph, Text, Title } from "./style"
+
+export function PresentationThree(){
+  return(
+    <Main>
+      <img src={ImgApresetationThree} alt=""/>
+      <Text>
+       <Title>Faça Sorteios</Title>      
+       <Paragraph>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever 
+       </Paragraph>
+       <ButtonApresentaion path="/apresentacao2" />   
+      </Text> 
+    </Main>    
+  );   
 }
--- src/Pages/Presentation/PresentationThree/style.ts
+++ src/Pages/Presentation/PresentationThree/style.ts
@@ -1,1 +1,35 @@
 import styled from "styled-components";
+
+export const Main = styled.div
+`
+max-width: 800px;
+height: 100vh;
+width: 100%;
+margin: 0 auto;
+padding: 0px 18px;
+display: flex;
+flex-direction: column;
+justify-content: flex-end;
+align-items: center;
+`
+export const Img = styled.img`
+max-width: 355px;
+width: 100%;
+`
+
+export const Text = styled.div`
+  margin-bottom: 3.75rem;
+  padding: 0px;
+`
+export const Title = styled.h1`
+font-size: 2rem;
+text-align: center;
+margin-top: 1.875rem;
+color: ${(props) => props.theme["green-500"]};
+`
+export const Paragraph = styled.p`
+
+margin-bottom: 5.25rem;
+text-align: center;
+font-size: 0.875em;
+`
