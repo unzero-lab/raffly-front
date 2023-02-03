a795505e4a71cfb7d8a8a2ce34404552ddae87a9
voncardoso <vonharrison26@gmail.com>
2023-02-03 20:19:09 -0300

    adjust resizing on splash screens

--- src/Pages/Presentation/PresentationTwo/style.ts
+++ src/Pages/Presentation/PresentationTwo/style.ts
@@ -1,40 +1,62 @@
-import { Center } from "@chakra-ui/react";
 import styled from "styled-components";
 
-export const Main = styled.main`
-  max-width: 800x;
-  height: 100vg
-  width: 100%
+export const Main = styled.div`
+  max-width: 800px;
+  width: 100%;
   margin: 0 auto;
-  padding: 0px 18px;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  align-items: center;
+  display: grid;
+  grid-template-columns: 1fr;
+  justify-content: center;
+  align-items: end;
+  height: 100vh;
+  padding: 0 18px;
 `;
 
 export const Img = styled.img`
-  max-width: 355px;
+  max-width: 600px;
   width: 100%;
+  margin: 0 auto;
+  align-self: center;
+
+  @media (min-width: 1000px) {
+    max-width: 500px;
+    width: 100%;
+  }
 `;
-
-
 export const Text = styled.div`
-  margin-bottom: 3.75rem;
+  //margin-bottom: 3.75rem;
   padding: 0px;
+  align-self: start;
 `;
-
 
 export const Title = styled.h1`
   font-size: 2rem;
   text-align: center;
   margin-top: 1.875rem;
   color: ${(props) => props.theme["green-500"]};
+
+  @media (min-height: 750px) {
+    font-size: 2.5rem;
+  }
+  @media (min-height: 920px) {
+    font-size: 4rem;
+  }
 `;
 
 export const Paragraph = styled.p`
-  margin-top: 1.2rem;
-  margin-bottom: 5.25rem;
-  text-align: Center;
+  text-align: center;
   font-size: 0.875rem;
+  margin-top: 0.875rem;
+  margin-bottom: 20px;
+  @media (min-height: 750px) {
+    font-size: 1.25rem;
+  }
+
+  @media (min-height: 920px) {
+    font-size: 1.5rem;
+  }
+
+  @media (min-width: 1000px) {
+    margin-bottom: 40px;
+  }
 `;
--- src/components/ButtonApresentaion/style.ts
+++ src/components/ButtonApresentaion/style.ts
@@ -7,4 +7,13 @@ export const Button = styled.button`
   border: none;
   color: ${(props) => props.theme["backgorund"]};
   border-radius: 6px;
+  margin-bottom: 3.75rem;
+  cursor: pointer;
+  @media (min-height: 750px) {
+    font-size: 1.25rem;
+  }
+
+  @media (min-height: 920px) {
+    font-size: 1.5rem;
+  }
 `;
