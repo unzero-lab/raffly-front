c6dd3f311b76e5c6f0accb88470196f789715ae8
voncardoso <vonharrison26@gmail.com>
2023-02-03 21:44:20 -0300

    lib react-icons reamove

--- src/Pages/RegistrationScreen/index.tsx
+++ src/Pages/RegistrationScreen/index.tsx
@@ -1,7 +1,14 @@
-import ImgRegistration from "../../assets/Raffly.svg"
-import React, { useState } from 'react';
-import { Form, InputContainer, Input, Button, Img, Main, Paragraph } from './style';
-import { FaEnvelope, FaLock } from 'react-icons/fa';
+import ImgRegistration from "../../assets/Raffly.svg";
+import React, { useState } from "react";
+import {
+  Form,
+  InputContainer,
+  Input,
+  Button,
+  Img,
+  Main,
+  Paragraph,
+} from "./style";
 
 const RegistrationScreen = () => {
   return (
@@ -10,22 +17,14 @@ const RegistrationScreen = () => {
       <Paragraph>Crie seu cadastro</Paragraph>
       <Form>
         <InputContainer>
-          <FaEnvelope />
-          <Input
-            type="email"
-            placeholder="E-mail"
-          />
+          <Input type="email" placeholder="E-mail" />
         </InputContainer>
         <InputContainer>
-          <FaLock />
-          <Input
-            type="password"
-            placeholder="Password"
-          />
+          <Input type="password" placeholder="Password" />
         </InputContainer>
         <Button type="submit">Criar seu cadastro</Button>
-      </Form>     
-      <Img/>
+      </Form>
+      <Img />
     </Main>
   );
 };
