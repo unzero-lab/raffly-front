def62114acae9da9299463e0295641b08cfe4763
Ludmila <ludmilapaulagarcia@gmail.com>
2023-02-03 19:24:34 -0500

    feat: created registration screnn

--- src/Pages/Presentation/PresentationTwo/style.ts
+++ src/Pages/Presentation/PresentationTwo/style.ts
@@ -1,62 +1,40 @@
+import { Center } from "@chakra-ui/react";
 import styled from "styled-components";
 
-export const Main = styled.div`
+export const Main = styled.main`
   max-width: 800px;
-  width: 100%;
+  height: 100vg
+  width: 100%
   margin: 0 auto;
-  display: grid;
-  grid-template-columns: 1fr;
-  justify-content: center;
-  align-items: end;
-  height: 100vh;
-  padding: 0 18px;
+  padding: 0px 18px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
 `;
 
 export const Img = styled.img`
-  max-width: 600px;
+  max-width: 355px;
   width: 100%;
-  margin: 0 auto;
-  align-self: center;
+`;
 
-  @media (min-width: 1000px) {
-    max-width: 500px;
-    width: 100%;
-  }
-`;
+
 export const Text = styled.div`
-  //margin-bottom: 3.75rem;
+  margin-bottom: 3.75rem;
   padding: 0px;
-  align-self: start;
 `;
+
 
 export const Title = styled.h1`
   font-size: 2rem;
   text-align: center;
   margin-top: 1.875rem;
   color: ${(props) => props.theme["green-500"]};
-
-  @media (min-height: 750px) {
-    font-size: 2.5rem;
-  }
-  @media (min-height: 920px) {
-    font-size: 4rem;
-  }
 `;
 
 export const Paragraph = styled.p`
-  text-align: center;
+  margin-top: 1.2rem;
+  margin-bottom: 5.25rem;
+  text-align: Center;
   font-size: 0.875rem;
-  margin-top: 0.875rem;
-  margin-bottom: 20px;
-  @media (min-height: 750px) {
-    font-size: 1.25rem;
-  }
-
-  @media (min-height: 920px) {
-    font-size: 1.5rem;
-  }
-
-  @media (min-width: 1000px) {
-    margin-bottom: 40px;
-  }
 `;
