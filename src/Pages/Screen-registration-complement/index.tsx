99f403f57b8efcbb739a73483d199087d1b91629
voncardoso <vonharrison26@gmail.com>
2023-02-10 11:35:56 -0300

    feat: completed registration add-on page

--- src/Pages/Screen-registration-complement/index.tsx
+++ src/Pages/Screen-registration-complement/index.tsx
@@ -1,5 +1,28 @@
-import { Main } from "./style";
+import { Button, Header, Input, InputGroupState, Main, Title } from "./style";
+import Avatar from "../../assets/Avatar.png";
 
 export function ScreenRegistrationComplement() {
-  return <Main>Tela de complemento</Main>;
+  return (
+    <Main>
+      <Title>Preencha seu perfil</Title>
+      <Header>
+        <img src={Avatar} alt="" />
+        <p>Adicionar foto</p>
+      </Header>
+
+      <form action="">
+        <Input type="text" placeholder="Nome" />
+        <Input type="text" placeholder="Nome" />
+        <Input type="text" placeholder="Nome" />
+
+        <InputGroupState>
+          <Input type="text" placeholder="Nome" />
+          <Input type="text" placeholder="UF" />
+        </InputGroupState>
+
+        <Input type="text" placeholder="Nome" />
+        <Button>Confirmar</Button>
+      </form>
+    </Main>
+  );
 }
--- src/Pages/Screen-registration-complement/style.ts
+++ src/Pages/Screen-registration-complement/style.ts
@@ -1,3 +1,59 @@
 import styled from "styled-components";
 
-export const Main = styled.section``;
+export const Main = styled.section`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 20px 40px;
+`;
+
+export const Title = styled.h2`
+  font-size: 1rem;
+  align-self: flex-start;
+  font-weight: 600;
+`;
+
+export const Header = styled.div`
+  margin-top: 40px;
+
+  p {
+    text-align: center;
+    margin-top: 1rem;
+    margin-bottom: 30px;
+  }
+`;
+
+export const Form = styled.form``;
+
+export const Input = styled.input`
+  border: 1px solid #dcdcdc;
+  border-radius: 6px;
+  width: 100%;
+  padding: 13px 16px;
+
+  margin-bottom: 16px;
+
+  &::placeholder {
+    color: ${(props) => props.theme["placeholder"]};
+  }
+`;
+
+export const InputGroupState = styled.div`
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  gap: 16px;
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  border-radius: 6px;
+  border: none;
+  color: ${(props) => props.theme["white"]};
+  background: ${(props) => props.theme["green-500"]};
+
+  margin-top: 14px;
+  margin-bottom: 20px;
+`;
