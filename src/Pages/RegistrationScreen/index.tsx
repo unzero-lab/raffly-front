55312a8c878cd63bcd2fe8a8b692355c33bc98ef
voncardoso <vonharrison26@gmail.com>
2023-02-10 19:20:50 -0300

    adjust icons

--- src/Pages/RegistrationScreen/index.tsx
+++ src/Pages/RegistrationScreen/index.tsx
@@ -1,33 +1,38 @@
-import ImgRegistration from "../../assets/Raffly.svg"
-import { Form, Button, InputContainer, Img, Main, Paragraph, InputBox } from './style';
+import ImgRegistration from "../../assets/Raffly.svg";
+import {
+  Form,
+  Button,
+  InputContainer,
+  Img,
+  Main,
+  Paragraph,
+  InputBox,
+} from "./style";
 import InputComponent from "../../components/InputComponent";
 import { InputIcon } from "../../components/InputIcon";
-import * as Icon from "phosphor-react";
+import { Envelope, Lock } from "phosphor-react";
 import { ButtonRegistration } from "../../components/ButtonRegistration";
 import { ButtonApresentaion } from "../../components/ButtonApresentaion";
 
-
 const RegistrationScreen = () => {
-
   return (
     <Main>
       <Img src={ImgRegistration} alt="imagem de cadastro" />
       <Paragraph>Crie seu cadastro</Paragraph>
       <InputContainer>
+        <Form>
+          <InputBox>
+            <InputIcon icon={<Envelope size={26} />} />
+            <InputComponent type="text" placeholder="Email" />
+          </InputBox>
 
-      <Form>
-        <InputBox>
-        <InputIcon icon={<Icon.Envelope size={26}/>}/>
-        <InputComponent type="text" placeholder="Email" />
-        </InputBox>
+          <InputBox>
+            <InputIcon icon={<Lock size={26} color="#A0AEC0" />} />
+            <InputComponent type="password" placeholder="Password" />
+          </InputBox>
 
-        <InputBox>
-        <InputIcon icon={<Icon.Lock size={26} color="#A0AEC0"/>}/>
-        <InputComponent type="password" placeholder="Password" />
-        </InputBox>
-        
-        <ButtonRegistration path="/"></ButtonRegistration>
-      </Form>
+          <ButtonRegistration path="/"></ButtonRegistration>
+        </Form>
       </InputContainer>
       <Img />
     </Main>
