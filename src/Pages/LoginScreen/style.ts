e121fe0a7695b1a23f579ecda7f06a0ad2f66d0b
Adriel Escorcio <adrielescorcio@gmail.com>
2023-02-13 21:54:10 -0300

    feat: added to logo and adjusted positioning

--- src/Pages/LoginScreen/style.ts
+++ src/Pages/LoginScreen/style.ts
@@ -1,33 +1,38 @@
 import styled from "styled-components";
 
-
 export const Main = styled.div`
-      max-width: 800px;
+      max-width: 500px;
       width: 100%;
       display: flex;
-      justify-content: end;
+      justify-content: center;
       align-items: center;
       justify-self: center;
       flex-direction: column;
       margin: 0 auto;
-      width: 300px;
-      height: 400px;
+      height: 100vh;
 `;
 
 export const Form = styled.form`
+  width: 100%;
+  padding: 20px;
   display: flex;
   gap: 20px;
   flex-direction: column;
 `;
 
-export const InputContainer = styled.div`
-  
 
+export const Img = styled.img`
+  max-width: 150px;
+  margin-bottom: 30px;
+`;
+
+export const InputContainer = styled.div`
+  width: 100%;
 `;
 
 export const Input = styled.input`
-
-  padding: 10px;
+  width: 100%;
+  padding: 15px 20px;
   border: none;
   font-size: 18px;
   color: ${(props) => props.theme["gray-500"]};
@@ -35,12 +40,11 @@ export const Input = styled.input`
 
 
 export const Button = styled.button`
-
-width: 100%;
-background: ${(props) => props.theme["green-500"]};
-border: none;
-border-radius: 6px;
-padding: 10px;
-color: ${(props) => props.theme["white"]};;
-margin-top: 30px;
+  width: 100%;
+  background: ${(props) => props.theme["green-500"]};
+  border: none;
+  border-radius: 6px;
+  padding: 10px;
+  color: ${(props) => props.theme["white"]};;
+  margin-top: 30px;
 `;
