ebd8327cf023a3b4a7f86f236d2e3e96d48ae71f
Adriel Escorcio <adrielescorcio@gmail.com>
2023-02-15 22:53:06 -0300

    feat: Screen login complete

--- src/Pages/LoginScreen/index.tsx
+++ src/Pages/LoginScreen/index.tsx
@@ -1,6 +1,9 @@
 import ImgLogo from "../../assets/Raffly.svg";
 import { useNavigate } from "react-router";
-import { Main, Img, Form, InputContainer, Input, Button } from "./style";
+import { Main, Img, Paragraph, Form, InputContainer,ContainerLinkCheck, Link, ContainerCheck, Check, Label, Button,Footer, Span, LinkCad} from "./style";
+import { InputIcon } from "../../components/InputIcon";
+import * as Icon from "phosphor-react";
+import InputComponent from "../../components/InputComponent";
 
 export function LoginScreen() {
   const navigate = useNavigate();
@@ -11,17 +14,33 @@ export function LoginScreen() {
   return (
     <Main>
       <Img src={ImgLogo} alt="Logo Raffy"/>
+      <Paragraph>Faça seu Login</Paragraph>
       <Form>
         <InputContainer>
-          <Input type="email" placeholder="E-mail"></Input>
+          <InputIcon icon={<Icon.Envelope size={26} color="#A0AEC0" />} />
+          <InputComponent type="email" placeholder="E-mail" />
         </InputContainer>
+
         <InputContainer>
-          <Input type="password" placeholder="Senha"></Input>
+          <InputIcon icon={<Icon.Lock size={26} color="#A0AEC0" />} />
+          <InputComponent type="password" placeholder="Senha" />
         </InputContainer>
+
+        <ContainerLinkCheck>
+          <Link href="http://">Esqueceu a senha?</Link>
+          <ContainerCheck>
+            <Check type="checkbox"></Check>
+            <Label>Lembre-me</Label>
+          </ContainerCheck>
+        </ContainerLinkCheck>
+        
         <Button onClick={handlenavigate} type="submit">
           Login
         </Button>
       </Form>
+      <Footer>
+        <Span>Não tem cadastro? <LinkCad>Cadastre-se</LinkCad></Span>
+      </Footer>
     </Main>
   );
 }
