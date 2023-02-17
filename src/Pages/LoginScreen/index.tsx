103ae922caa2556b46683aab36965d1bea025cc9
voncardoso <vonharrison26@gmail.com>
2023-02-16 21:35:04 -0300

    chore: json-server

--- src/Pages/LoginScreen/index.tsx
+++ src/Pages/LoginScreen/index.tsx
@@ -18,9 +18,17 @@ import {
   Input,
 } from "./style";
 import { Envelope, Lock } from "phosphor-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export function LoginScreen() {
   const navigate = useNavigate();
+  const [login, setLogin] = useState("");
+  const { register, handleSubmit } = useForm();
+
+  function handleLogin(data: any) {
+    console.log("senhs", data);
+  }
 
   function handlenavigate() {
     navigate("/cadastro");
@@ -29,15 +37,24 @@ export function LoginScreen() {
     <Main>
       <Img src={ImgLogo} alt="Logo Raffy" />
       <Paragraph>Faça seu Login</Paragraph>
-      <Form>
+      <Form onSubmit={handleSubmit(handleLogin)}>
         <InputContainer>
           <Envelope size={26} color="#A0AEC0" />
-          <Input type="email" placeholder="E-mail" />
+          <Input
+            type="email"
+            placeholder="E-mail"
+            value={login}
+            onChange={(event) => setLogin(event.target.value)}
+          />
         </InputContainer>
 
         <InputContainer>
           <Lock size={26} color="#A0AEC0" />
-          <Input type="password" placeholder="Senha" />
+          <Input
+            type="password"
+            placeholder="Senha"
+            {...register("password")}
+          />
         </InputContainer>
 
         <ContainerLinkCheck>
@@ -48,9 +65,7 @@ export function LoginScreen() {
           </ContainerCheck>
         </ContainerLinkCheck>
 
-        <Button onClick={handlenavigate} type="submit">
-          Login
-        </Button>
+        <Button type="submit">Login</Button>
       </Form>
       <Footer>
         <Span>
