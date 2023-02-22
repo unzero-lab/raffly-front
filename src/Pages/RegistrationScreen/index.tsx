e1a201ef7e6319dd69a4f3642156947c0639b58b
Ludmila <ludmilapaulagarcia@gmail.com>
2023-02-21 19:25:25 -0500

    feat: function to register with react-hook-form

--- src/Pages/RegistrationScreen/index.tsx
+++ src/Pages/RegistrationScreen/index.tsx
@@ -10,30 +10,42 @@ import {
   Input,
 } from "./style";
 import { Envelope, Lock } from "phosphor-react";
+import { useForm } from "react-hook-form";
 
-const RegistrationScreen = () => {
+export function RegistrationScreen () {
+  const { register, handleSubmit } = useForm()
+
+  function handleRegistration(data: any) {
+    console.log(data);
+  }
+
   return (
     <Main>
       <Img src={ImgRegistration} alt="imagem de cadastro" />
       <Paragraph>Crie seu cadastro</Paragraph>
       <InputContainer>
-        <Form>
+      <Form onSubmit={handleSubmit(handleRegistration)}>
           <InputBox>
             <Envelope size={26} color="#A0AEC0" />
-            <Input type="text" placeholder="Email" />
+            <Input 
+              type="email" 
+              placeholder="Email" 
+              {...register("email")} />
           </InputBox>
 
           <InputBox>
             <Lock size={26} color="#A0AEC0" />
-            <Input type="password" placeholder="Password" />
+            <Input 
+              type="password" 
+              placeholder="Password" 
+              {...register("password")}
+            />
           </InputBox>
 
-          <Button>Criar seu cadastro</Button>
+          <Button type="submit">Criar seu cadastro</Button>
         </Form>
       </InputContainer>
       <Img />
     </Main>
   );
 };
-
-export default RegistrationScreen;
